0b8f406553b4ddd96302724bd40a2a8028266168
Vishal <chinchanevishal03@gmail.com>
2021-12-07 09:53:11 +0530

    adedTitleBar

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function (e) {
 
     let addTxt = document.getElementById("addTxt");
+    addTitle = document.getElementById("addTitle");
     let notes = localStorage.getItem("notes")
     if (notes == null) {
         notesObj = [];
@@ -12,9 +13,15 @@ addBtn.addEventListener("click", function (e) {
     else {
         notesObj = JSON.parse(notes);
     }
-    notesObj.push(addTxt.value);
+
+    let myObj = {
+        title: addTitle.value,
+        text: addTxt.value
+      }
+    notesObj.push(myObj);
     localStorage.setItem("notes", JSON.stringify(notesObj));
     addTxt.value = "";
+    addTitle.value = "";
     // console.log(notesObj)
     showNotes();
 
@@ -40,8 +47,8 @@ function showNotes() {
         html += `
         <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
         <div class="card-body">
-          <h5 class="card-title">Note ${index + 1}</h5>
-          <p class="card-text">${element}</p>
+          <h5 class="card-title">${element.title}</h5>
+          <p class="card-text">${element.text}</p>
           <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
         </div>
       </div>`;
